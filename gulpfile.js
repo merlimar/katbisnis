ceb460fc5758b0cc006b188a4d0fdd57a71a8dea
merlimar <merli.marvin@gmail.com>
2017-02-09 16:15:49 +0100

    Gulp task for images

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,18 @@
 var gulp = require("gulp");
+	image = require("gulp-image");
+	sass = require("gulp-sass");
 // --- Task for images
 gulp.task( "images", function(){
 	gulp.src("src/images/**")
 		.pipe( image())
-		.pipe(gulp.assets('./assets'));
+		.pipe(gulp.dest('assets/images'));
 })
 // --- Task for styles
-
+gulp.task( "css", function(){
+	gulp.src( "src/sass/**/*.scss")
+	.pipe( sass().on( "error", sass.logError))
+	.pipe( gulp.dest( "assets/css",))
+})
 // --- Task for pug
 
 // --- Task for js
