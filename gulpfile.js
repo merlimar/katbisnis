b9614a4d4121fd74d83ca73839e4172ab7e35284
merlimar <merli.marvin@gmail.com>
2017-02-09 16:08:26 +0100

    Gulp task for images

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 var gulp = require("gulp");
 // --- Task for images
-
+gulp.task( "images", function(){
+	gulp.src("src/images/**")
+		.pipe( image())
+		.pipe(gulp.assets('./assets'));
+})
 // --- Task for styles
 
 // --- Task for pug
