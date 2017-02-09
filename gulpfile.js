539ec643cc681208fbbb24aa2fbb3a0fcfd4a805
merlimar <merli.marvin@gmail.com>
2017-02-09 16:38:49 +0100

    Gulp task for pug

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,13 @@ var gulp = require("gulp");
 	sass = require("gulp-sass");
 	autoprefixer = require( "gulp-autoprefixer");
 	csso = require("gulp-csso");
+	pug = require("gulp-pug");
 // --- Task for images
 gulp.task( "images", function(){
 	gulp.src("src/images/**")
 		.pipe( image())
 		.pipe(gulp.dest('assets/images'));
-})
+});
 // --- Task for styles
 gulp.task( "css", function(){
 	gulp.src( "src/sass/**/*.scss")
@@ -16,9 +17,13 @@ gulp.task( "css", function(){
 	.pipe( autoprefixer())
 	.pipe(csso())
 	.pipe( gulp.dest( "assets/css"))
-})
+});
 // --- Task for pug
-
+gulp.task( "html", function(){
+	gulp.src("src/pug/**/*.pug")
+	.pipe( pug({}))
+	.pipe(gulp.dest("."));
+});
 // --- Task for js
 
 // --- Watch tasks
